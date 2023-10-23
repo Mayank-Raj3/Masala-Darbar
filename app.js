3e196c419403731888390817fa26eed6cfefd47a
Mayank-Raj3 <guptamannu884@gmail.com>
2023-10-24 03:38:58 +0530

    Made Resturant card

--- app.js
+++ app.js
@@ -1,51 +1,105 @@
-// imported react and reactdom from nodemodule folder
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "heading"
-);
-const heading1 = (
-  <h1 id="title" key="h2">
-    Namaste react
-  </h1>
-);
-// const heading1 = React.createElement(
-//   "h1",
-//   {
-//     id: "title",
-//     key: "h2"
-//   },
-//   "heading1"
-// );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
-// root.render(container);
-
-//functional component
-/*
-  Name of component starts with capital letter  
- */
-const Title = () => <h1>This is title </h1>;
-
-const HeaderComponent = () => (
-  <>
-    // {} inside this we can write any javascrit {1 + 2 + 3} {Title()}
-    <Title />
-    <h1>Namste react Functional component </h1>;
-  </>
+const Title = () => (
+  <a className="a-logo" href="/">
+    <img
+      src="https://masaladarbar.in/wp-content/uploads/2023/03/cropped-Screenshot_2022-11-22-12-32-05-91_965bbf4d18d205f782c6b8409c5773a4-removebg-preview.png"
+      className="logo"
+      alt="Masala Darbar"
+      decoding="async"
+      sizes="(max-width: 633px) 100vw, 633px"
+    />
+  </a>
 );
 
-// console.log()
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Offers</li>
+          <li>Help</li>
+          <li>Profile</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="cards-container">
+      <div className="card">
+        <div className="card-media">
+          <img
+            className="card-img"
+            src="https://images.unsplash.com/photo-1615719413546-198b25453f85?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzMDQwMjkwNA&ixlib=rb-1.2.1&q=85"
+          />
+
+          <div className="discount">
+            40% OFF<p>Up to ₹80</p>
+          </div>
+          <div className="pro-discount">PRO extra 15% OFF</div>
+          <div className="delivery-time">39 mins</div>
+          <div className="bookmark"></div>
+        </div>
+        <div className="card-description">
+          <div className="about-place">
+            <div className="place">
+              <p className="place-name">Pizza Hut</p>
+              <p className="place-speciality">Italian, Fast Food, Beverages</p>
+            </div>
+            <div className="place-review">
+              <p className="rating">4.6 ★</p>
+              <p className="per-person">₹200 per one</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <>
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+    </>
+  );
+};
+
+const Footer = () => {
+  return <div className="footer"></div>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
